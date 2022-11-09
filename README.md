bc53b2f690dc8815189ff1cade227de2c317b71d
ImJongHoon <imjh990614@gmail.com>
2022-11-09 21:31:00 +0900

    PMYK-10 <index.html and select.html animation 1.0.0>

--- README.md
+++ README.md
@@ -63,3 +63,27 @@ https://codepen.io/minaalfy/pen/exgvjb
     animation: painting3_reduce 1.5s ease-in-out forwards;
     animation-delay: 3s;
 }
+
+index페이지 아래부분
+select페이지 버튼 애니메이션
+select페이지 선택 애니메이션
+
+일단, 가상선택자(pseudo selector)에는 가상 클래스(pseudo class)와 가상 엘리먼트(pseudo element)가 있습니다.
+
+엄밀히 따지면 가상 선택자는 없습니다만, 편의상 많이 사용되는 말입니다.
+
+가상클래스에는 :link, :visited, :hover, :active, :first-child, :nth-child 등이 있고,
+가상엘리먼트에는 ::first-letter, ::first-line, ::before, ::after 등이 있습니다.
+
+CSS 2.1에서는 가상클래스, 가상엘리먼트 모두 싱글 콜론을 사용합니다.
+
+하지만 CSS3 부터는 좀 더 명확히 분류하기 위해서, 가상 클래스에는 싱글콜론을, 가상 엘리먼트에는 더블콜론을 사용합니다.
+
+브라우저 개발사들은 CSS2.1을 먼저 지원하고, CSS3도 지원을 계속 추가하고 있습니다.
+CSS3에 대한 지원을 추가하더라도 이전 스펙에 있던 속성들도 함께 계속 지원합니다.
+
+그래야 하위 호환성이 보장되기 때문입니다.
+브라우저를 새버전으로 업데이트 했더니 예전에 잘 보이던 페이지가 안보이면 곤란하니까요.
+표현에 치우친 네이밍이라는 이유로 HTML5에선 사라진(deprecated) font, center, strike와 같은 태그들을 여전히 브라우저에서 지원하는 것도 같은 이유에서 입니다.
+
+이런 이유로 모던 브라우저들에서는 싱글콜론을 사용한 가상엘리먼트와 더블콜론을 사용한 경우를 모두 지원하고 있습니다.
--- static/js/index.js
+++ static/js/index.js
@@ -1,12 +1,39 @@
 const indexHeader = document.querySelector(".index-header");
+const indexMain = document.querySelector(".index-main");
+
+const title1 = indexMain.querySelector(".play-game-title-container");
+const text1 = indexMain.querySelector(".play-game-main-text");
+const title2 = indexMain.querySelector(".make-game-title-container");
+const text2 = indexMain.querySelector(".make-game-main-text");
+const title3 = indexMain.querySelector(".block-chain-title-container");
+const text3 = indexMain.querySelector(".block-chain-main-container");
+
+
 
 function changeOpacity(){
-    let opacity = window.scrollY / 500;
+    let opacity = window.scrollY / 400;
     indexHeader.style.backgroundColor = `rgba(34, 40, 49, ${opacity})`;
 }
 
 function main(){
-    window.addEventListener('scroll', changeOpacity)
+    window.addEventListener('scroll', changeOpacity);
+
+    //observe 객체 만들기
+    let observer = new IntersectionObserver((e)=>{
+        e.forEach((element)=>{
+            if(element.isIntersecting){
+                element.target.style.opacity = 1;
+                element.target.style.transform = "translate(0,0)";
+            }
+        })
+    })
+
+    observer.observe(title1);
+    observer.observe(text1);
+    observer.observe(title2);
+    observer.observe(text2);
+    observer.observe(title3);
+    observer.observe(text3);
 }
 
 main();
