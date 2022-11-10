bef1ef29afd1d4204f254cbcb0d1b472f0c761d0
ImJongHoon <imjh990614@gmail.com>
2022-11-10 22:38:34 +0900

    PMYK-10 index page btn animation finish

--- static/js/index.js
+++ static/js/index.js
@@ -7,16 +7,23 @@ const title2 = indexMain.querySelector(".make-game-title-container");
 const text2 = indexMain.querySelector(".make-game-main-text");
 const title3 = indexMain.querySelector(".block-chain-title-container");
 const text3 = indexMain.querySelector(".block-chain-main-container");
-
-
+const btn1 = indexMain.querySelector(".go-to-select-btn");
 
 function changeOpacity(){
     let opacity = window.scrollY / 400;
     indexHeader.style.backgroundColor = `rgba(34, 40, 49, ${opacity})`;
 }
+function delayLoad(){
+    setInterval(loadBtn, 2200);
+}
+function loadBtn(){
+    btn1.classList.add('display-flex');
+    btn1.classList.remove('display-none');
+}
 
 function main(){
     window.addEventListener('scroll', changeOpacity);
+    document.addEventListener("DOMContentLoaded", delayLoad);
 
     //observe 객체 만들기
     let observer = new IntersectionObserver((e)=>{
@@ -34,6 +41,7 @@ function main(){
     observer.observe(text2);
     observer.observe(title3);
     observer.observe(text3);
+    observer.observe(btn1);
 }
 
 main();
